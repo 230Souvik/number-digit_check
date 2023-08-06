4f4ebc2dbe1a01ebce595a14d84b914b8377c6f8
Souvik Saha <135532224+230Souvik@users.noreply.github.com>
2023-08-06 16:57:52 +0530

    Add files via upload

---
+++ Ex22.java
@@ -0,0 +1,34 @@
+
+package ex22;
+import java.util.*;
+
+public class Ex22 {
+public static int findAutoCount (String n) 
+    {
+        int sum = 0;
+        for (int i = 0; i < n.length (); i++)
+            sum = sum + Integer.parseInt (n.charAt (i) + "");
+        if (sum == n.length ())
+        {
+            int count = 0;
+            int arr[] = new int[10];
+            for (int i = 0; i < n.length (); i++)
+                arr[Integer.parseInt (n.charAt (i) + "")]++;
+
+            for (int i = 0; i < arr.length; i++)
+                if (arr[i] != 0)
+                    count++;
+            return count;
+        }
+        else
+            return 0;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner sc = new Scanner (System.in);
+        String str = sc.next ();
+        System.out.println (findAutoCount (str));
+    }
+    
+}
